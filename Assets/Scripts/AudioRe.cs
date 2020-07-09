33071208311af206b7e2bb9898b8302f666e12fa
BondhanKimbalazani <aobond2@gmail.com>
2020-07-09 08:58:44 +0100

    Copied over script from urp

---
+++ Assets/Scripts/AudioRe.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class AudioRe : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    //public int FrequencyNumber;
+    public static float[] _freqBand = new float[8];
+    public static float[] _bandBuffer = new float[8];
+    public float BufferDecreaseDown = 0.005f;
+    public float BufferDecreaseUp = 1.2f;
+    float[] _bufferDecrease = new float[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; g++)
+        {
+            if (_freqBand [g] > _bandBuffer [g])
+            {
+                _bandBuffer[g] = _freqBand[g];
+                _bufferDecrease[g] = BufferDecreaseDown;
+            }
+
+            if (_freqBand [g] < _bandBuffer[g])
+            {
+                _bufferDecrease[g] = (_bandBuffer[g] - _freqBand[g]) / 8;
+                _bandBuffer[g] -= _bufferDecrease[g];
+                //_bandBuffer[g] -= _bufferDecrease[g];
+                //_bufferDecrease[g] *= BufferDecreaseUp;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeInstantiate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInstantiate : MonoBehaviour
+{
+    public GameObject _sampleCubePrefab;
+    public float _cubeDistance = 100f;
+    public float _maxScale = 1000;
+    public float _cubeSize = 1;
+    GameObject[] _sampleCube = new GameObject[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.parent = this.transform;
+            _instanceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, (-360f / 512f) * i, 0);
+            _instanceSampleCube.transform.position = Vector3.forward * _cubeDistance;
+            _sampleCube[i] = _instanceSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (_sampleCube != null)
+            {
+                _sampleCube[i].transform.localScale = new Vector3(_cubeSize , (AudioRe._samples[i] * _maxScale) + 2, _cubeSize);
+            }
+        }
+    }
+}
